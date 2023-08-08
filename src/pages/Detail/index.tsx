7930750d814b6a2b0e7a2bad62a2f3ce85ddd350
Pcjmy <1716045191@qq.com>
2023-08-09 00:38:54 +0800

    feat: Tabs组件拆分

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,7 +1,8 @@
+import InterfaceDetail from '@/components/InterfaceDetail';
 import MockService from '@/components/MockService';
 import { CloseOutlined } from '@ant-design/icons';
-import type { DescriptionsProps, TabsProps } from 'antd';
-import { Button, Descriptions, Input, Table, Tabs } from 'antd';
+import type { TabsProps } from 'antd';
+import { Button, Input, Tabs } from 'antd';
 import styles from './index.less';
 
 const InterfacePage: React.FC = () => {
@@ -9,6 +10,7 @@ const InterfacePage: React.FC = () => {
     {
       key: '1',
       label: '接口详情',
+      children: <InterfaceDetail />,
     },
     {
       key: '2',
@@ -21,123 +23,7 @@ const InterfacePage: React.FC = () => {
     {
       key: '4',
       label: 'Mock服务',
-    },
-  ];
-
-  const tabsItems: DescriptionsProps['items'] = [
-    {
-      key: '1',
-      label: '接口名称',
-      children: '初始接口1',
-    },
-    {
-      key: '2',
-      label: '状态',
-      children: '已完成',
-    },
-    {
-      key: '3',
-      label: '接口路径',
-      children: '/api/admin/add',
-    },
-    {
-      key: '4',
-      label: 'Mock地址',
-      children: 'https://pines.ca/mock/100/api/admin/add',
-    },
-    {
-      key: '5',
-      label: '创建人',
-      children: '张三',
-    },
-    {
-      key: '6',
-      label: '更新时间',
-      children: '2023-08-01 20:45:12',
-    },
-  ];
-
-  const columns1 = [
-    {
-      title: '参数名称',
-      dataIndex: 'paraname',
-      key: 'paraname',
-    },
-    {
-      title: '类型',
-      dataIndex: 'type',
-      key: 'type',
-    },
-    {
-      title: '是否必须',
-      dataIndex: 'isNecessary',
-      key: 'isNecessary',
-    },
-    {
-      title: '示例',
-      dataIndex: 'example',
-      key: 'example',
-    },
-    {
-      title: '备注',
-      dataIndex: 'note',
-      key: 'note',
-    },
-  ];
-
-  const columns2 = [
-    {
-      title: '参数名称',
-      dataIndex: 'paraname',
-      key: 'paraname',
-    },
-    {
-      title: '参数值',
-      dataIndex: 'paravalue',
-      key: 'paravalue',
-    },
-    {
-      title: '是否必须',
-      dataIndex: 'isNecessary',
-      key: 'isNecessary',
-    },
-    {
-      title: '默认值',
-      dataIndex: 'default',
-      key: 'default',
-    },
-    {
-      title: '备注',
-      dataIndex: 'note',
-      key: 'note',
-    },
-  ];
-
-  const columns3 = [
-    {
-      title: '名称',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '类型',
-      dataIndex: 'type',
-      key: 'type',
-    },
-    {
-      title: '是否必须',
-      dataIndex: 'isNecessary',
-      key: 'isNecessary',
-    },
-    {
-      title: '默认值',
-      dataIndex: 'default',
-      key: 'default',
-    },
-    {
-      title: '备注',
-      dataIndex: 'note',
-      key: 'note',
+      children: <MockService />,
     },
   ];
 
@@ -160,28 +46,6 @@ const InterfacePage: React.FC = () => {
       </div>
       <div className={styles['detail-container-right']}>
         <Tabs defaultActiveKey="1" items={items} />
-        <div className={styles['basic-info']}>
-          <h2>基本信息</h2>
-          <Descriptions
-            className={styles['basic-info-description']}
-            column={2}
-            items={tabsItems}
-          />
-        </div>
-        <div className={styles['request-para']}>
-          <h2>请求参数</h2>
-          <div className={styles['request-para-data']}>
-            <h3>Headers:</h3>
-            <Table columns={columns1} />
-            <h3 className={styles['data-body']}>Body:</h3>
-            <Table columns={columns2} />
-          </div>
-        </div>
-        <div className={styles['return-data']}>
-          <h2>返回数据</h2>
-          <Table className={styles['return-data-table']} columns={columns3} />
-        </div>
-        <MockService />
       </div>
     </div>
   );
